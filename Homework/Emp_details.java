d92ba9956f9de278e5ccaf5e10f487d6a877aabf
Surya Ajay Patel <113435336+Surya4277@users.noreply.github.com>
2022-12-08 22:27:01 +0530

    Update Emp_details.java

--- Homework/Emp_details.java
+++ Homework/Emp_details.java
@@ -1,10 +1,15 @@
+**Write a program  to create a process which will store the employee details and calculate the employee gross salary and net salary.
+
+	
 package Surya;
 
 import java.util.Scanner;
 
 public class Emp_details 
 {
+	//create a scanner
 	Scanner sc = new Scanner(System.in);
+	//create a variable
 	int emp_id, emp_age, contact_no;
 	String emp_name, emp_address;
 	
