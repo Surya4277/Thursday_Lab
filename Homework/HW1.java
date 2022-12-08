ee4410c3eccadb8775d3596fe9c5538214ccde30
Surya Ajay Patel <113435336+Surya4277@users.noreply.github.com>
2022-12-08 22:28:18 +0530

    Update HW1.java

--- Homework/HW1.java
+++ Homework/HW1.java
@@ -1,3 +1,6 @@
+**Write a program  to create a process which will store the employee details and calculate the employee gross salary and net salary.
+
+
 package Surya;
 
 public class HW1 extends gross_net
@@ -5,7 +8,7 @@ public class HW1 extends gross_net
 
 	public static void main(String[] args) 
 	{
-		HW1 obj = new HW1();
+		HW1 obj = new HW1();//create a object and calling
 		obj.accept();
 		obj.calc();
 
